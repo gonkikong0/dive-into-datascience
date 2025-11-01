9ad4036b12eee3b87593e5f56baa70a53a632fd3
gonkikong0 <toshivy@outlook.com>
2025-11-01 20:09:08 +0530

    Custom subsets

--- check_hours.py
+++ check_hours.py
@@ -4,8 +4,13 @@ hour = pd.read_csv('hour.csv')
 
 # print(hour['count'].mean()) 
 # print(hour['count'].median())
-# print(hour['count'].std())
+# print(hour['count'].std())    #Summary Statistics
 # print(hour['count'].min())
 # print(hour['count'].max())
 
-print(hour.describe())
+# print(hour.describe()) #Summary Statistics 
+
+# print(hour.loc[2:4,'count']) #Loc() Used to specify a subset [<row>,<column>]
+
+print(hour.loc[hour['hr'] < 5, 'registered'].mean())
+print(hour.loc[hour['hr'] < 5, 'casual'].mean())
