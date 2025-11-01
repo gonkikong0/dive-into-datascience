7bb888415fe1d665f57478396d50715beb3641ff
gonkikong0 <toshivy@outlook.com>
2025-11-01 18:31:36 +0530

    Summary Statistics

--- check_hours.py
+++ check_hours.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 hour = pd.read_csv('hour.csv')
-print(hour.head())
+
+print(hour['count'].mean()) 
+print(hour['count'].median())
+print(hour['count'].std())
+print(hour['count'].min())
+print(hour['count'].max())
