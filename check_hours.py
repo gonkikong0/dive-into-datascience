741e8fdf32e5288b6e0eefa3ff422369a1ab5615
gonkikong0 <toshivy@outlook.com>
2025-11-03 01:54:07 +0530

    Multiple Groupings

--- check_hours.py
+++ check_hours.py
@@ -17,6 +17,10 @@ hour = pd.read_csv('hour.csv')
 # print(hour.loc[hour['hr'] < 5, 'registered'].mean())
 # print(hour.loc[hour['hr'] < 5, 'casual'].mean())
 
-print(hour.loc[(hour['hr']< 5) & (hour['temp'] < .50), 'count'].mean())
-print(hour.loc[(hour['hr']< 5) & (hour['temp'] > .50), 'count'].mean())
-      
+# print(hour.loc[(hour['hr']< 5) & (hour['temp'] < .50), 'count'].mean()) #Analyzing subsets -- (Nighttime Data)
+# print(hour.loc[(hour['hr']< 5) & (hour['temp'] > .50), 'count'].mean())  # 0 Represents very cold and 1 represents very warm temperature.
+
+#SEASON --> 1 for winter, 2 for spring, 3 for summer, 4 for fall >
+
+# print(hour.groupby(['season'])['count'].mean())
+print(len(hour.groupby(['season', 'holiday'])['count'].mean()))
